43c429775d7c7bc4f5ea7e9456544d932ee123d5
Mohammed Tahir Siddique <mohammedsiddique729@gmail.com>
2026-08-18 11:37:43 +0530

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        # count all chars
+        count = defaultdict(int) # ch:count
+        for ch in s:
+            count[ch] += 1
+
+        result = []
+        onGoing = {}
+        ending = -1
+        for idx, each in enumerate(s):
+            if each in onGoing:
+                onGoing[each] += 1
+            else:
+                onGoing[each] = 1
+                ending = max(ending, s.rfind(each))
+
+            if ending == idx:
+                result.append(sum(list(onGoing.values())))
+                onGoing = {}
+                ending = -1
+        
+        return result
